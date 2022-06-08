c878cd4c690b8cf2f9a7cfbf02d3e9f0c5f86509
Suman Islam <sumanislam1416@gmail.com>
2022-06-08 13:01:22 +0600

    apply mutaion in front-end

--- client/src/containers/track-card.js
+++ client/src/containers/track-card.js
@@ -3,6 +3,24 @@ import styled from '@emotion/styled';
 import { colors, mq } from '../styles';
 import { humanReadableTimeFromSeconds } from '../utils/helpers';
 import { Link } from '@reach/router';
+import {gql, useMutation} from '@apollo/client'
+
+// Mutation to increment a track's number of views
+// (exported for test)
+
+export const INCREMENT_TRACK_VIEWS = gql`
+  mutation IncrementTrackViews($incrementTrackViewsId: ID!) {
+  incrementTrackViews(id: $incrementTrackViewsId) {
+    code
+    success
+    message
+    track {
+      id
+      numberOfViews
+    }
+  }
+}
+`
 
 /**
  * Track Card component renders basic info in a card format
@@ -11,8 +29,17 @@ import { Link } from '@reach/router';
 const TrackCard = ({ track }) => {
   const { title, thumbnail, author, length, modulesCount, id } = track;
 
+  const [incrementTrackViews] = useMutation(INCREMENT_TRACK_VIEWS, {
+    variables: {
+      incrementTrackViews: id
+    },
+    onCompleted: (data) => {
+      console.log(data);
+    }
+  })
+
   return (
-    <CardContainer to={`/track/${id}`}>
+    <CardContainer to={`/track/${id}`} onClick={incrementTrackViews}>
       <CardContent>
         <CardImageContainer>
           <CardImage src={thumbnail} alt={title} />
